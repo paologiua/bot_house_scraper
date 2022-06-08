7a556e968cd36107d9cf503f0db910efd13be436
Paolo Giua <paolo.giuaa@gmail.com>
2022-06-09 00:02:24 +0200

    Now get more information from immobiliare.it

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def load_json_from_file(file_name, default_format):
         with open(os.path.dirname(os.path.realpath(__file__)) + '/%s' % file_name) as f:
             return json.load(f)
     except:
-        write_json_from_file(file_name, default_format)
+        write_json_to_file(file_name, default_format)
         return default_format
 
-def write_json_from_file(file_name, dictionary):
+def write_json_to_file(file_name, dictionary):
     with open(os.path.dirname(os.path.realpath(__file__)) + '/%s' % file_name, 'w') as f:
         json.dump(dictionary, f, indent = 4)
 
@@ -29,7 +29,7 @@ def send_everyone(bot, message):
                 if "chat not found" in str(e):
                     options_dict = load_json_from_file("options.json", OPTIONS_DEFAULT_FORMAT)
                     options_dict["chat_ids"].remove(chat_id)
-                    write_json_from_file("options.json", options_dict)
+                    write_json_to_file("options.json", options_dict)
                     message_not_sent = False
                 else:
                     print("Error sending to %s, retry in 3 seconds ..." % chat_id)
@@ -73,6 +73,9 @@ STATUS_DEFAULT_FORMAT = {
 API_TOKEN = load_json_from_file("options.json", OPTIONS_DEFAULT_FORMAT)['api_token']
 URLS = load_json_from_file("options.json", OPTIONS_DEFAULT_FORMAT)['urls']
 
+MIN_PRICE = 200
+MAX_PRICE = 550
+
 bot = telebot.TeleBot(API_TOKEN)
 
 pid = os.fork()
@@ -87,7 +90,7 @@ Ciao sono BotHouseScraper e ti aggiornerò sulle nuove case!
         options_dict = load_json_from_file("options.json", OPTIONS_DEFAULT_FORMAT)
         if(message.chat.id not in options_dict['chat_ids']):
             options_dict['chat_ids'].append(message.chat.id)
-            write_json_from_file("options.json", options_dict)
+            write_json_to_file("options.json", options_dict)
 
     # Handle '/status'
     @bot.message_handler(commands=['status'])
@@ -126,9 +129,24 @@ else:
     import time
     from datetime import datetime, timedelta
 
-    def get_page(driver, url):
+    def get_page(url):
+
+        options = webdriver.ChromeOptions()
+
+        options.add_argument('--headless')
+        options.add_argument('window-size=1920x1080')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument('--disable-blink-features=AutomationControlled')
+
+        #Open Browser
+        driver = webdriver.Chrome('chromedriver', options=options)
+
         driver.get(url)
         content = (WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.TAG_NAME, "html"))).get_attribute('outerHTML'))
+        driver.close()
         return BeautifulSoup(content, "html.parser")
 
     def get_anticaptcha_url(url):
@@ -140,8 +158,8 @@ else:
 
         return "https://" + netloc.replace(".", "-") + ".translate.goog" + path + "?" + query + "&_x_tr_sl=it&_x_tr_tl=it&_x_tr_hl=it&_x_tr_pto=op,wapp"
 
-    def get_anticaptcha_page(driver, url):
-        return get_page(driver, get_anticaptcha_url(url))
+    def get_anticaptcha_page(url):
+        return get_page(get_anticaptcha_url(url))
 
     def get_n_time_ago(time, time_format):
         d = datetime.today()
@@ -215,31 +233,106 @@ else:
         except:
             return ["None", "None"]
 
+    def get_phone_from_image_url(image_url):
+        try:
+            from PIL import Image
+            import requests
+            from io import BytesIO
+
+            import pytesseract
+            import cv2
+            import numpy as np
+
+            response = requests.get(image_url)
+            col = Image.open(BytesIO(response.content))
+            gray = col.convert('L')
+            bw = gray.point(lambda x: 0 if x < 128 else 255, '1')
+
+            # Make Numpy/OpenCV-compatible version
+            image = np.array(bw.convert('RGB'))
+
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            thresh = 255 - cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+            # Blur and perform text extraction
+            thresh = cv2.GaussianBlur(thresh, (3, 3), 0)
+
+            div = cv2.divide(gray, thresh, scale=192)  # added
+
+            data = pytesseract.image_to_string(div, config='--psm 11 digits')
+            return data.strip()
+        except:
+            return "None"
+
+
+    def get_immobiliare_info(url):
+        room_page = get_anticaptcha_page(url)
+
+        try:
+            sex_ideal_tenant = list(filter(lambda x: "genere" in x.text, room_page.select(".im-features__list")))[0].select(".im-features__value")[0].text.strip()
+            if(sex_ideal_tenant == "Femmina"):
+                return False
+        except:
+            pass
+
+        info = []
+        
+        try:
+            phone_image = room_page.select(".im-lead__phone > img")
+            phone = get_phone_from_image_url(get_anticaptcha_url(phone_image[0]["src"])) if phone_image else "None"
+            info.append(phone)
+        except:
+            info.append("None")
+
+        try:
+            date = list(filter(lambda x: "riferimento e Data annuncio" in x.text, room_page.select(".im-features__list")))[0].select(".im-features__value")[0].text.split(" - ")[1].strip()
+            info.append(date)
+        except:
+            info.append("None")
+
+        return info
+
     def get_text_el(el):
-        return el.text if el else "None"
+        return el.text if el and el.text else "None"
 
     def dict_dif(dict1, dict2):
         diff = list(set(dict1) - set(dict2))
 
         return {key: value for key, value in dict1.items() if key in diff}
     
-    def sort_rooms_by_date(rooms_dict):
+    def sort_rooms_by_time_and_date(rooms_dict):
         return {k: v for k, v in sorted(rooms_dict.items(), reverse=True, key=lambda item: -1 if item[1][3] == "None" or item[1][4] == "None" else time.mktime(datetime.strptime(item[1][3] + "|" + item[1][4], "%H:%M|%d/%m/%Y").timetuple()))}
     
+    def sort_rooms_by_date(rooms_dict):
+        return {k: v for k, v in sorted(rooms_dict.items(), reverse=True, key=lambda item: -1 if item[1][4] == "None" else time.mktime(datetime.strptime(item[1][4], "%d/%m/%Y").timetuple()))}
+    
+    def check_price_range(strprice, min_price, max_price):
+        try:
+            price = int("".join(filter(lambda x: x.isdigit(), strprice)))
+            if(f'''{price}''' in strprice):
+                return (price >= min_price and price <= max_price)
+        except:
+            pass
+
+        return True
+
     def get_rooms_from_idealista():
         # Idealista
         rooms_data = {}
 
-        page = get_anticaptcha_page(driver, URLS['idealista'])
+        page = get_anticaptcha_page(URLS['idealista'])
 
         rooms = page.select(".item-multimedia-container:not(.item_contains_branding)")
         for room in rooms:
             link = room.find("a", {"class": "item-link"})
 
             room_url = "https://www.idealista.it%s" % urllib.parse.urlparse(link["href"]).path
-            room_name = link.get_text()
+            room_name = get_text_el(link)
 
             room_price = get_text_el(room.find("span", {"class": "item-price"}))
+            if(not check_price_range(room_price, MIN_PRICE, MAX_PRICE)):
+                continue
+
             room_phone = get_text_el(room.find("span", {"class": "icon-phone"}))
             
             pub_info = get_idealista_publication_info(room)
@@ -257,7 +350,7 @@ else:
         status_dict = load_json_from_file('status.json', STATUS_DEFAULT_FORMAT)
         status_dict['idealista']['last_check'] = rooms_data
         status_dict['idealista']['last_check_time'] = now_time
-        write_json_from_file('status.json', status_dict)
+        write_json_to_file('status.json', status_dict)
         
         for url in list(reversed(new_houses.keys())):
             send_everyone(bot, room_to_str(new_houses, url))
@@ -266,18 +359,21 @@ else:
         #Subito
         rooms_data = {}
 
-        page = get_anticaptcha_page(driver, URLS['subito'] + "&gndr=1")
+        page = get_anticaptcha_page(URLS['subito'] + "&gndr=1")
         man_rooms = list(filter(lambda x: not x.select(".PostingTimeAndPlace-module_vetrina-badge__XWWCm"), page.select(".BigCard-module_link__kVqPE")))
         
-        page = get_anticaptcha_page(driver, URLS['subito'] + "&gndr=3")
+        page = get_anticaptcha_page(URLS['subito'] + "&gndr=3")
         both_sexes_rooms = list(filter(lambda x: not x.select(".PostingTimeAndPlace-module_vetrina-badge__XWWCm"), page.select(".BigCard-module_link__kVqPE")))
 
         rooms = both_sexes_rooms + man_rooms
         for room in rooms:
             room_url = "https://www.subito.it%s" % urllib.parse.urlparse(room["href"]).path
-            room_name = room.find("h2", {"class": "BigCard-module_card-title__Cgcnt"}).get_text()
+            room_name = get_text_el(room.find("h2", {"class": "BigCard-module_card-title__Cgcnt"}))
 
             room_price = get_text_el(room.find("p", {"class": "price"}))
+            if(not check_price_range(room_price, MIN_PRICE, MAX_PRICE)):
+                continue
+
             room_phone = "None"
             
             pub_info = get_subito_publication_info(room)
@@ -285,7 +381,7 @@ else:
             publication_date = pub_info[1]
 
             rooms_data[room_url] = [room_name, room_price, room_phone, publication_time, publication_date]
-        rooms_data = sort_rooms_by_date(rooms_data)
+        rooms_data = sort_rooms_by_time_and_date(rooms_data)
 
         now_time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
         print("[%s]:" % now_time)
@@ -296,7 +392,7 @@ else:
         status_dict = load_json_from_file('status.json', STATUS_DEFAULT_FORMAT)
         status_dict['subito']['last_check'] = rooms_data
         status_dict['subito']['last_check_time'] = now_time
-        write_json_from_file('status.json', status_dict)
+        write_json_to_file('status.json', status_dict)
         
         for url in list(reversed(new_houses.keys())):
             send_everyone(bot, room_to_str(new_houses, url))
@@ -305,56 +401,66 @@ else:
         # Immobiliare
         rooms_data = {}
 
-        page = get_page(driver, URLS['immobiliare'])
+        page = get_page(URLS['immobiliare'])
+        indifferent_roommates_rooms = list(filter(lambda x: not x.select(".nd-figure"), page.select(".nd-mediaObject__content")))
+        
+        page = get_page(URLS['immobiliare'] + "&sessoInquilini=M")
+        male_roommates_rooms = list(filter(lambda x: not x.select(".nd-figure"), page.select(".nd-mediaObject__content")))
 
-        rooms = list(filter(lambda x: not x.select(".nd-figure"), page.select(".nd-mediaObject__content")))
+        page = get_page(URLS['immobiliare'] + "&sessoInquilini=F")
+        female_roommates_rooms = list(filter(lambda x: not x.select(".nd-figure"), page.select(".nd-mediaObject__content")))
+
+        page = get_page(URLS['immobiliare'] + "&sessoInquilini=E")
+        male_female_roommates_rooms = list(filter(lambda x: not x.select(".nd-figure"), page.select(".nd-mediaObject__content")))
+
+        rooms = indifferent_roommates_rooms + male_roommates_rooms + female_roommates_rooms + male_female_roommates_rooms
+  
+        last_check = load_json_from_file('status.json', STATUS_DEFAULT_FORMAT)['immobiliare']['last_check']
         for room in rooms:
             link = room.find("a", {"class": "in-card__title"})
-
             room_url = link["href"]
-            room_name = link.get_text()
+            
+            if(room_url in last_check):
+                continue
 
-            room_price = room.find("li", {"class": "in-realEstateListCard__features--main"}).get_text()
-            room_phone = "None"
+            room_name = get_text_el(link)
+
+            room_price = get_text_el(room.find("li", {"class": "in-realEstateListCard__features--main"}))
+            if(not check_price_range(room_price, MIN_PRICE, MAX_PRICE)):
+                continue
+                
+            room_info = get_immobiliare_info(room_url)
+            if(not room_info):
+                continue
+
+            room_phone = room_info[0]
             
             publication_time = "None"
-            publication_date = "None"
+            publication_date = room_info[1]
 
             rooms_data[room_url] = [room_name, room_price, room_phone, publication_time, publication_date]
+        rooms_data = sort_rooms_by_date(rooms_data)
 
         now_time = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
         print("[%s]:" % now_time)
         
-        new_houses = dict_dif(rooms_data, load_json_from_file('status.json', STATUS_DEFAULT_FORMAT)['immobiliare']['last_check'])
-        print(json.dumps(new_houses,indent = 4) + "\n")
+        new_houses = dict_dif(rooms_data, last_check)
+        print(json.dumps(new_houses, indent = 4) + "\n")
 
         status_dict = load_json_from_file('status.json', STATUS_DEFAULT_FORMAT)
         status_dict['immobiliare']['last_check'] = rooms_data
         status_dict['immobiliare']['last_check_time'] = now_time
-        write_json_from_file('status.json', status_dict)
+        write_json_to_file('status.json', status_dict)
         
         for url in list(reversed(new_houses.keys())):
             send_everyone(bot, room_to_str(new_houses, url))
 
-    options = webdriver.ChromeOptions()
-
-    options.add_argument('--headless')
-    options.add_argument('window-size=1920x1080')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)
-    options.add_argument('--disable-blink-features=AutomationControlled')
-
-    #Open Browser
-    driver = webdriver.Chrome('chromedriver', options=options)
-
     while(True):
         get_rooms_from_idealista()
-        time.sleep((10 + random.randint(-3, 3)) * 60)
+        time.sleep((8 + random.randint(-3, 3)) * 60)
 
         get_rooms_from_subito()  
-        time.sleep((10 + random.randint(-3, 3)) * 60)
+        time.sleep((8 + random.randint(-3, 3)) * 60)
         
         get_rooms_from_immobiliare()
-        time.sleep((10 + random.randint(-3, 3)) * 60)
+        time.sleep((8 + random.randint(-3, 3)) * 60)
--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install chromium-driver
+sudo apt-get install tesseract-ocr
 
 pip install pyTelegramBotAPI
 pip install selenium
+
+pip install opencv-python
+pip install pytesseract
