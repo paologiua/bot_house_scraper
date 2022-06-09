56b849210b27885009177b1dd0506c9d1dc72eb3
Paolo Giua <paolo.giuaa@gmail.com>
2022-06-09 02:09:26 +0200

    Now get more information from immobiliare.it

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,12 @@
 sudo apt-get install chromium-driver
 sudo apt-get install tesseract-ocr
 
+sudo apt-get install libatlas-base-dev 
+sudo apt-get install libjasper-dev 
+sudo apt-get install libqtgui4 
+sudo apt-get install python3-pyqt5 
+sudo apt install libqt4-test
+
 pip install pyTelegramBotAPI
 pip install selenium
 
